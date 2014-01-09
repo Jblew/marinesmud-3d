50d9726b7b96d465b9bc5b5966230b28fa055ff5
Jblew <jedrzejblew@gmail.com>
2014-01-09 23:44:12 +0100

    Add movement to train

---
+++ Assets/HopeTrainLampController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HopeTrainLampController : MonoBehaviour {
+
+	public GameObject offZoneTrigger;
+
+	// Use this for initialization
+	void Start () {
+		//GetComponent<Light> ().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject == offZoneTrigger) {
+			GetComponent<Light> ().enabled = false;
+			//gameObject.transform.parent.gameObject.renderer.enabled = false;
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.gameObject == offZoneTrigger) {
+			GetComponent<Light> ().enabled = true;
+			//gameObject.transform.parent.gameObject.renderer.enabled = true;
+		}
+	}
+}
---
+++ Assets/Scripts/Train/HopeTrainController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HopeTrainController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector3(0f, 0f, 10f*Time.deltaTime));
+	}
+}
