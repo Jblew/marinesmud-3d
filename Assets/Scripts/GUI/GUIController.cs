add76abe8588a069284b66fc3c6370f89637d12b
Jblew <jedrzejblew@gmail.com>
2014-02-11 13:42:34 +0100

    Add muting

--- Assets/Scripts/GUI/GUIController.cs
+++ Assets/Scripts/GUI/GUIController.cs
@@ -8,6 +8,7 @@ public class GUIController : MonoBehaviour {
 	public float angle = 0;
 	public float lastAngle = 0;
 	public Vector2 size = new Vector2(128, 128);
+	public bool soundOn = true;
 
 	private Vector2 pos = new Vector2(0, 0);
 	private Rect rect;
@@ -37,10 +38,25 @@ public class GUIController : MonoBehaviour {
 		}
 		else player = GameObject.FindGameObjectWithTag (Tags.PLAYER);
 
+		if (soundOn) {
+			if (GUI.Button (new Rect (Screen.width-80, Screen.height - 30, 75, 25), "Mute")) {
+				soundOn = false;
+				AudioListener.volume = 0;
+			}
+		} else {
+			if (GUI.Button (new Rect (Screen.width-80, Screen.height - 30, 75, 25), "Unmute")) {
+				soundOn = true;
+				AudioListener.volume = 100;
+			}
+		}
+
 		if (i % 50 == 0 && guiLoginPanel != null) {
 			if(guiLoginPanel.correctLogin != null) transform.FindChild("UsernameText").gameObject.GetComponent<GUIText>().text = guiLoginPanel.correctLogin;
 			if(guiLoginPanel.selectedCharacter != null) transform.FindChild("CharacterNameText").gameObject.GetComponent<GUIText>().text = guiLoginPanel.selectedCharacter["name"];
 		}
+
+
+
 		i++;
 	}
 }
