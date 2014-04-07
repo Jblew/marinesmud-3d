1c7f04e0f928fe6f3c76f3e3867d24fbf7b45039
Jblew <jedrzejblew@gmail.com>
2014-04-08 00:06:17 +0200

    Add right hand grab distance range calculation

--- Assets/Scripts/Player/PlayerMouseMovement.cs
+++ Assets/Scripts/Player/PlayerMouseMovement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class PlayerMouseMovement : MonoBehaviour {
 	public float mouseTurnSpeed = 1f;
-	public Transform desiredRightHandPosition = null;
+	private Vector3 desiredRightHandPosition = Vector3.zero;
 	
 	private GUILoginPanel guiLoginPanelScript = null;
 	
@@ -11,9 +11,16 @@ public class PlayerMouseMovement : MonoBehaviour {
 	private Vector3 moveRotation = Vector3.zero;
 	private Rigidbody myRigidbody = null;
 	private Transform head = null;
+	private Transform rightShoulder = null;
 	private Transform rightArm = null;
 	private Transform rightForearm = null;
 	private Transform rightHand = null;
+	private Transform pointer1 = null;
+
+	public float shoulderLength = 0f;
+	public float armLength = 0f;
+	public float forearmLength = 0f;
+	public string grabStatus = "Unknown";
 	
 	void Start () {
 		GameObject alternativMUDClient = GameObject.FindWithTag ("AlternativMUDClient");
@@ -22,12 +29,15 @@ public class PlayerMouseMovement : MonoBehaviour {
 		} else {
 			Debug.LogWarning ("PlayerMovement: Cannot find by tag: #AlternativMUDClient");
 		}
+		GameObject _pointer1 = GameObject.FindGameObjectWithTag ("Pointer1");
+		if (_pointer1 != null) pointer1 = _pointer1.transform;
 
 		head = Util.FindChildWithName(transform, "Head");
+		rightShoulder = Util.FindChildWithName(transform, "RightShoulder");
 		rightArm = Util.FindChildWithName(transform, "RightArm");
 		rightForearm = Util.FindChildWithName(transform, "RightForeArm");
 		rightHand = Util.FindChildWithName(transform, "RightHand");
-		if(head != null && rightArm && rightForearm != null && rightHand != null) {
+		if(head != null && rightShoulder != null && rightArm != null && rightForearm != null && rightHand != null) {
 			Debug.Log ("Got UMA rigs");
 		}
 		else {
@@ -42,11 +52,25 @@ public class PlayerMouseMovement : MonoBehaviour {
 	}
 
 	void LateUpdate() {
-		if(head != null && rightArm && rightForearm != null && rightHand != null) {
+		if(pointer1 != null) desiredRightHandPosition = pointer1.position;
+		if(head != null && rightShoulder != null && rightArm != null && rightForearm != null && rightHand != null) {
 			//neck.eulerAngles = new Vector3(0f, 0f, -90f);
 			/***==TOP==**/
-			float distanceFromArmToDesiredPosition = Vector3.Distance(rightArm.position, desiredRightHandPosition.position);
-			head.localEulerAngles = new Vector3(0f, 0f, 0f);
+			shoulderLength = Vector3.Distance(rightShoulder.position, rightArm.position);
+			armLength = Vector3.Distance(rightArm.position, rightForearm.position);
+			forearmLength = Vector3.Distance(rightForearm.position, rightHand.position);
+
+			float grabDistance = Vector3.Distance(rightShoulder.position, desiredRightHandPosition);
+			if(grabDistance > shoulderLength+armLength+forearmLength) {
+				grabStatus = "Too far";
+
+			}
+			else {
+				grabStatus = "Distance ok";
+			}
+
+			//head.localEulerAngles = new Vector3(0f, 0f, 0f);
+
 		}
 	}
 }
